e488485cef3f00df8a4b93deb557c1bc2e42208c
NobeliY <nobeliylord@gmail.com>
2023-03-08 13:55:24 +0300

    Lab 1 (IP-Address) - v 0.2

--- IP-Address/src/main.py
+++ IP-Address/src/main.py
@@ -1,7 +1,6 @@
 import string
-from typing import Union
 
-from src.mask import Mask, build_binary
+from mask import Mask, build_binary
 
 
 class Singleton(object):
@@ -16,28 +15,15 @@ class Singleton(object):
 class MyIP(Singleton):
 
     def __init__(self, uname: str):
-
-        self._binary_gateway: str = ""
-        self.__binary_network = None
-        self._binary_wildcard = None
-        self._broadcast: Union[dict[str], None] = None
-        self._hosts: dict = {}
-        self.__raw_gateway: list[int] = []
-        self.__raw_network: list[int] = []
-        self._gateway = None
-        self._ip_address_binary = None
-        self._network = None
-        self._mask = None
-        self._split_mask: list[int] = []
-        self._wildcard: list[int] = []
         self._string_ip_address = None
-        self._raw_ip_address = None
         self._ip_address = None
-        self._raw_str_binary = None
-        self._prefix: int = -1
+        self._raw_ip_address = None
+        self._ip_address_binary = None
         self._str_binary = None
-        self._binary_list = None
-        self._index_list = None
+        self._raw_str_binary = None
+
+        self._cached_: dict = {}
+
         if uname:
             self.uname = list(uname.lower()[:7])
         else:
@@ -62,37 +48,43 @@ class MyIP(Singleton):
         ]
 
     @property
-    def gateway(self):
-        if not self._gateway:
-            self.gateway = self.__raw_network
-        return self._gateway
+    def gateway(self) -> dict:
+        _gateway = self._cached_.get("gateway")
+        if not _gateway:
+            self.gateway = self.network["raw"]
+        return self._cached_["gateway"]
 
     @gateway.setter
     def gateway(self, raw_network: list[int]):
-        if not self.__raw_network:
-            self.network = str(self.mask.netmask).split(".")
-        self.__raw_gateway = raw_network
-        self.__raw_gateway[-1] += 1
-        self._binary_gateway = ".".join(map(str, [build_binary(item) for item in self.__raw_gateway]))
-        self._gateway = ".".join(map(str, self.__raw_gateway))
+        _raw_gateway = raw_network
+        _raw_gateway[-1] += 1
+        _binary_gateway = ".".join(map(str, [build_binary(item) for item in _raw_gateway]))
+        _gateway = ".".join(map(str, _raw_gateway))
+        self._cached_["gateway"]: dict = {
+            "bin": _binary_gateway,
+            "dec": _gateway,
+            "raw": _raw_gateway
+        }
+        del _binary_gateway, _gateway, _raw_gateway
 
     @property
-    def hosts(self):
-        if not self._hosts:
+    def hosts(self) -> dict:
+        _hosts = self._cached_.get("hosts")
+        if not _hosts:
             self.hosts = self.prefix
-        return self._hosts["count"]
+        return self._cached_["hosts"]
 
     @hosts.setter
     def hosts(self, prefix: int):
-        __host_min = list(map(int, self.gateway.split(".")))
+        __host_min = self.gateway["raw"]
         __host_min[-1] += 1
-        __broadcast = [self.__raw_network[i] + self._wildcard[i] for i in range(0, 4)]
+        __broadcast = [self.network["raw"][i] + self.wildcard["dec"][i] for i in range(0, 4)]
         __broadcast[-1] -= 1
         __host_max = [
             item if __broadcast.index(item) != 3 else item - 1
             for item in __broadcast
         ]
-        self._hosts: dict = {
+        self._cached_["hosts"]: dict = {
             "count": 2 ** (32 - prefix) - 2,
             "min": {
                 "bin": ".".join(map(str, [build_binary(item) for item in __host_min])),
@@ -103,17 +95,18 @@ class MyIP(Singleton):
                 "dec": __host_max.__str__()[1:-1].replace(', ', '.')
             }
         }
-        self._broadcast = {
+        self._cached_["broadcast"] = {
             "bin": ".".join(map(str, [build_binary(item) for item in __broadcast])),
             "dec": __broadcast.__str__()[1:-1].replace(', ', '.')
         }
         del __host_max, __host_min
 
     @property
-    def mask(self):
-        if not self._mask:
+    def mask(self) -> dict:
+        _mask = self._cached_.get("mask")
+        if not _mask:
             self.mask = self.prefix
-        return self._mask
+        return self._cached_["mask"]
 
     @mask.setter
     def mask(self, prefix: int):
@@ -122,14 +115,17 @@ class MyIP(Singleton):
             __mask__ = __mask_tuple__[-1]
         except KeyError:
             __mask__ = __mask_tuple__
-        self._mask = __mask__["dec"]
-        self._split_mask = list(map(int, __mask__["bin"].split(".")))
+        self._cached_["mask"]: dict = {
+            "dec": __mask__["dec"],
+            "split_mask": list(map(int, __mask__["bin"].split(".")))
+        }
 
     @property
-    def network(self):
-        if not self._network:
-            self.network = str(self.mask).split(".")
-        return self._network
+    def network(self) -> dict:
+        _network = self._cached_.get("network")
+        if not _network:
+            self.network = str(self.mask["dec"]).split(".")
+        return self._cached_["network"]
 
     @network.setter
     def network(self, mask_binary):
@@ -140,39 +136,44 @@ class MyIP(Singleton):
             mask_binary_parsed = f"{'0' * (8 - len(mask_binary_item))}{mask_binary_item}"
             for j in range(0, len(self._ip_address_binary[i])):
                 raw_network[i].append(str(int(self._ip_address_binary[i][j]) * int(mask_binary_parsed[j])))
-        network: list = []
+        network: list[int] = []
         binary_network: list = []
         for item in raw_network:
             binary_network.append("".join(item))
             network.append(int(binary_network[-1], 2))
-        self.__raw_network = network
-        self.__binary_network = ".".join(binary_network)
-        self._network = ".".join(map(str, network))
+        self._cached_["network"]: dict = {
+            "raw": network,
+            "bin": ".".join(binary_network),
+            "dec": ".".join(map(str, network))
+        }
         del binary_network, raw_network, network
 
     @property
-    def prefix(self):
-        if self._prefix == -1:
+    def prefix(self) -> int:
+        _prefix = self._cached_.get("prefix")
+        if not _prefix:
             self.prefix = self._raw_str_binary
-        return self._prefix
+        del _prefix
+        return self._cached_["prefix"]
 
     @prefix.setter
     def prefix(self, raw_str_binary: str):
-        if raw_str_binary:
-            self._prefix = int(raw_str_binary[-3:], 2) + 19
-        else:
-            self._prefix = 19
+        self._cached_["prefix"] = int(raw_str_binary[-3:], 2) + 19 if raw_str_binary else 19
 
     @property
-    def wildcard(self):
-        if not self._wildcard:
-            self.wildcard = list(map(int, self.mask.split(".")))
-        return self._wildcard
+    def wildcard(self) -> dict:
+        _wildcard = self._cached_.get("wildcard")
+        if not _wildcard:
+            self.wildcard = list(map(int, self.mask["dec"].split(".")))
+        return self._cached_["wildcard"]
 
     @wildcard.setter
     def wildcard(self, split_mask: list[int]):
-        self._wildcard = [255 - item for item in split_mask]
-        self._binary_wildcard: str = ".".join([build_binary(item) for item in self._wildcard])
+        self._cached_["wildcard"]: dict = {
+            "dec": [255 - item for item in split_mask]
+        }
+        self._cached_["wildcard"]["bin"]: str = ".".join([build_binary(item)
+                                                          for item in self._cached_["wildcard"]["dec"]])
 
     def return_u_code(self):
         return self._index_list
@@ -219,14 +220,14 @@ class MyIP(Singleton):
     def __str__(self):
         return f"a: Address: {self._string_ip_address} | {self._raw_ip_address}\n" \
                f"b: Class: {self.get_class_ip()} | Prefix: {self.prefix}\n" \
-               f"c: Mask: {self.mask} | {'.'.join(map(str, self._split_mask))}\n" \
-               f"d: WildCard: {self.wildcard.__str__()[1:-1].replace(', ', '.')} | {self._binary_wildcard}\n" \
-               f"e: Hosts: {self.hosts}\n" \
-               f"f: Network: {self.network} | {self.__binary_network}\n" \
-               f"g: Gateway: {self.gateway} | {self._binary_gateway}\n" \
-               f"h: HostMin: {self._hosts['min']['dec']} | {self._hosts['min']['bin']} \n" \
-               f"i: HostMax: {self._hosts['max']['dec']} | {self._hosts['max']['bin']}\n" \
-               f"j: Broadcast: {self._broadcast['dec']} | {self._broadcast['bin']}"
+               f"c: Mask: {self.mask['dec']} | {'.'.join(map(str, self.mask['split_mask']))}\n" \
+               f"d: WildCard: {self.wildcard['dec'].__str__()[1:-1].replace(', ', '.')} | {self.wildcard['bin']}\n" \
+               f"e: Hosts: {self.hosts['count']}\n" \
+               f"f: Network: {self.network['dec']} | {self.network['bin']}\n" \
+               f"g: Gateway: {self.gateway['dec']} | {self.gateway['bin']}\n" \
+               f"h: HostMin: {self.hosts['min']['dec']} | {self.hosts['min']['bin']} \n" \
+               f"i: HostMax: {self.hosts['max']['dec']} | {self.hosts['max']['bin']}\n" \
+               f"j: Broadcast: {self._cached_['broadcast']['dec']} | {self._cached_['broadcast']['bin']}"
 
 
 if __name__ == "__main__":
